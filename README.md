09d0dbf208107e231900328dc2412a7a9e0f5bdf
Andrei <b1naryth1ef@gmail.com>
2014-12-25 16:05:50 -0600

    Basics working, proper tests and some docs

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Shaker
 A D wrapper around libsodium for simple encryption, signing, and other awesome crypto swag. A WIP.
+
+## Encrypting Data
+```D
+auto alice = new BoxKeyPair;
+auto bob = new BoxKeyPair;
+
+EncryptedMessage message = alice.encrypt("Hey Bob, I think your sexy!", bob);
+bob.decrypt(message, alice); // "Hey Bob, I think your sexy!"
+```
+
+## Signing Data
+```D
+auto alice = new SignKeyPair;
+auto bob = new SignKeyPair;
+
+SignedMessage message = alice.sign("This is definitly alice talking!");
+message.signedBy(bob); // true
+```
--- src/shaker.d
+++ src/shaker.d
@@ -5,17 +5,17 @@ import std.stdio;
 extern (C) {
   int crypto_box_keypair(ref ubyte pk[32], ref ubyte sk[32]);
   int crypto_sign_keypair(ref ubyte pk[32], ref ubyte sk[64]);
-  int crypto_box_easy(ubyte*, ubyte*, ulong, ref ubyte[24], ref ubyte[32], ref ubyte[64]);
+  int crypto_box_easy(ubyte*, ubyte*, ulong, ref ubyte[24], ref ubyte[32], ref ubyte[32]);
   int crypto_sign(ubyte*, ulong*, ubyte*, ulong, ref ubyte[64]);
   int crypto_sign_open(ubyte*, ulong*, ubyte *, ulong, ref ubyte[32]);
-  int crypto_box_open_easy(ubyte*, ubyte*, ulong, ref ubyte[24], ref ubyte[32], ref ubyte[64]);
+  int crypto_box_open_easy(ubyte*, ubyte*, ulong, ref ubyte[24], ref ubyte[32], ref ubyte[32]);
 }
 
 struct SignedMessage {
   ubyte[] data;
 
   // signedBy returns true if this messages was signed by the keypair, signer.
-  bool signedBy(KeyPair signer) {
+  bool signedBy(SignKeyPair signer) {
     ubyte output[] = new ubyte[this.data.length - 64];
     ulong length;
     int valid = crypto_sign_open(&output[0], &length, &this.data[0], this.data.length, signer.public_key);
@@ -28,7 +28,7 @@ struct EncryptedMessage {
   ubyte[24] nonce;
 }
 
-class KeyPair {
+class SignKeyPair {
   ubyte public_key[32];
   ubyte secret_key[64];
 
@@ -51,19 +51,28 @@ class KeyPair {
     crypto_sign(&output[0], &length, &data[0], data.length, this.secret_key);
     return SignedMessage(output);
   }
+}
 
-  EncryptedMessage encrypt(string data, KeyPair other) {
+class BoxKeyPair {
+  ubyte public_key[32];
+  ubyte secret_key[32];
+
+  this() {
+    crypto_box_keypair(this.public_key, this.secret_key);
+  }
+
+  EncryptedMessage encrypt(string data, BoxKeyPair other) {
     return this.encrypt(cast(ubyte[])data, other);
   }
 
-  EncryptedMessage encrypt(ubyte[] data, KeyPair other) {
+  EncryptedMessage encrypt(ubyte[] data, BoxKeyPair other) {
     ubyte output[] = new ubyte[data.length + 16];
     ubyte nonce[24]; // TODO
     crypto_box_easy(&output[0], &data[0], data.length, nonce, other.public_key, this.secret_key);
     return EncryptedMessage(output, nonce);
   }
 
-  ubyte[] decrypt(EncryptedMessage msg, KeyPair other) {
+  ubyte[] decrypt(EncryptedMessage msg, BoxKeyPair other) {
     ubyte output[] = new ubyte[msg.message.length - 16];
     crypto_box_open_easy(&output[0], &msg.message[0], msg.message.length, msg.nonce, other.public_key, this.secret_key); 
     return output;
@@ -72,12 +81,18 @@ class KeyPair {
 }
 
 unittest {
-  KeyPair bob = new KeyPair;
-  KeyPair alice = new KeyPair;
+  auto bob = new SignKeyPair;
+  auto alice = new SignKeyPair;
+  auto jim = new BoxKeyPair;
+  auto alex = new BoxKeyPair;
 
   // Test that key signing works
   SignedMessage data = bob.sign("hey alice this is bob!");
   assert(data.signedBy(bob));
   assert(!data.signedBy(alice));
+
+  // Test that encryption works
+  EncryptedMessage emsg = jim.encrypt("hey alex, your password is 1", alex);
+  assert(alex.decrypt(emsg, jim) == "hey alex, your password is 1");
 }
 
